e241742dc7179be594a7e10de97f9abe73abb47e
nikiwright <ddwright@usc.edu>
2022-11-07 12:02:31 -0800

    monda nov 7: results + detail page update

---
+++ funclasses_detail.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+
+include 'nwloginvariables.php';
+
+$mysql = new mysqli(
+    $host,
+    $userid,
+    $userpw,
+    $db
+);
+
+if($mysql->connect_errno) {
+    echo "db connection error : " . $mysql->connect_error;
+    exit();
+}
+?>
+
+
+<html>
+<title> Details Page </title>
+<header>
+    <style>
+        body {
+            background-color: white;
+        }
+
+    </style>
+</header>
+<body>
+
+<?php
+
+if (empty($_REQUEST["recordid"])){
+    echo "Error! please go through the search page";
+    exit();
+}
+
+$sql = "SELECT * from generalView
+         WHERE fun_classes_id =" .
+    $_REQUEST['recordid'];
+
+echo "SQL: ". $sql. "<br>"."<br>";
+
+$results = $mysql -> query($sql);
+
+if(!$results){
+    echo "ERROR: " . $mysql -> error;
+}
+
+while ($currentrow = $results -> fetch_assoc()){
+    echo "<h1>". $currentrow["className"]. " Details" ."</h1>";
+    echo "<strong>"."Course Title: " ."</strong>". $currentrow["className"]. "<br>";
+    echo "<strong>"."CourseID: " ."</strong>". $currentrow["courseID"]. "<br>";
+    echo "<strong>"."About: " ."</strong>". $currentrow["classBio"]. "<br>";
+    echo "<strong>"."Days offered: " ."</strong>". $currentrow["weekday"]. "<br>";
+    echo "<strong>"."Units: " ."</strong>". $currentrow["unit_num"]. "<br>"."<br>";
+
+    echo "<strong>"."Class Department: " ."</strong>". $currentrow["classDepartment"]. "<br>";
+    echo "<strong>"."School: " ."</strong>". $currentrow["school"]. "<br>"."<br>";
+
+    echo "<strong>"."Instructor Name: " ."</strong>". $currentrow["instructorName"]. "<br>";
+    echo "<strong>"."Instructor Rating: " ."</strong>". $currentrow["instructorRating"]. "<br>"."<br>";
+
+    echo "<strong>"."Course Review: " ."</strong>". $currentrow["review"]. "<br>";
+}
+
+?>
+
+</body>
+</html>
--- funclasses_result.php
+++ funclasses_result.php
@@ -3,11 +3,6 @@ session_start();
 
 include 'nwloginvariables.php';
 
-$host = "??????";
-$userid = "??????";
-$userpw = "??????";
-$db = "??????";
-
 $mysql = new mysqli(
     $host,
     $userid,
@@ -30,13 +25,6 @@ if($mysql->connect_errno) {
         }
 
         #container{
-            width: 80%;
-            margin: auto;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-evenly;
-            flex-direction: column;
-            text-align: center;
 
             background-color: white;
             border: solid 1px lightgrey;
@@ -52,26 +40,65 @@ if($mysql->connect_errno) {
 
     <?php
 
-    $sql = 		"SELECT * FROM generalView WHERE 1=1";
-    $sql .= " AND className LIKE '%" .
-        $_REQUEST['className'] . "%'";
+    $sql = "SELECT * FROM generalView WHERE 1=1
+    AND courseID LIKE '%" . $_REQUEST["courseID"] . "%'
+     AND className LIKE '%" . $_REQUEST["className"] . "%'
+     AND classDepartment LIKE '%" . $_REQUEST["classDepartment"] . "%'
+      AND instructorName LIKE '%" . $_REQUEST["instructorName"] . "%'";
 
-    $sql .= " AND courseID LIKE '%" .
-        $_REQUEST['courseID'] . "%'";
-
-    $sql .= " AND classDepartment LIKE '%" .
-        $_REQUEST['classDepartment'] . "%'";
-
-    $sql .= " AND professorName LIKE '%" .
-        $_REQUEST['professorName'] . "%'";
-
-    if($_REQUEST['school'] != "ALL") {
-        $sql .= " AND rating ='" . $_REQUEST["school"] . "'";
+    if ($_REQUEST['school'] != "ALL") {
+        $sql .= " AND school_id ='" . $_REQUEST["school"] . "'";
+    }
+    if ($_REQUEST['interest'] != "ALL") {
+        $sql .= " AND interest_id ='" . $_REQUEST["interest"] . "'";
+    }
+    if ($_REQUEST['unit'] != "ALL") {
+        $sql .= " AND unit_id ='" . $_REQUEST["unit"] . "'";
     }
 
-    $sql .= " ORDER BY ". $_REQUEST['orderby'];
+    $x = 0;
+
+    //Check if checkbox is checked
+    if(!empty($_REQUEST['monday'])){
+        #Checkbox selected code
+        $x = 1;
+    }
+    if(!empty($_REQUEST['tuesday'])){
+        $x = 1;
+    }
+    if(!empty($_REQUEST['wednesday'])){
+        $x = 3;
+    }
+    if(!empty($_REQUEST['thursday'])){
+        $x = 4;
+    }
+    if(!empty($_REQUEST['friday'])){
+        $x = 5;
+    }
+    if(!empty(($_REQUEST['monday']) AND ($_REQUEST['wednesday']))){
+        $x = 6;
+    }
+    if(!empty(($_REQUEST['tuesday']) AND ($_REQUEST['thursday']))){
+        $x = 10;
+    }
+    if(!empty(($_REQUEST['monday']) AND ($_REQUEST['tuesday']) AND ($_REQUEST['wednesday']))){
+        $x = 11;
+    }
+    if(!empty(($_REQUEST['monday']) AND ($_REQUEST['tuesday']) AND ($_REQUEST['wednesday']) AND ($_REQUEST['friday']))){
+        $x = 8;
+    }
+    if(!empty(($_REQUEST['monday']) AND ($_REQUEST['tuesday']) AND ($_REQUEST['wednesday']) AND ($_REQUEST['thursday']))){
+        $x = 7;
+    }
+    if(!empty(($_REQUEST['monday']) AND ($_REQUEST['tuesday']) AND ($_REQUEST['wednesday']) AND ($_REQUEST['thursday']) AND ($_REQUEST['friday']))){
+        $x = 9;
+    }
+
+    $sql .= " AND weekday_id = $x";
+
 
     $results = $mysql->query($sql);
+    echo "<hr>Your SQL:<br> " . $sql . "<br><br>";
 
     if(!$results) {
         echo "<hr>Your SQL:<br> " . $sql . "<br><br>";
@@ -79,10 +106,6 @@ if($mysql->connect_errno) {
         exit();
     }
 
-    // echo "<em>You searched for Title: " . $_REQUEST['title'] . " and Rating: " . $_REQUEST['rating'] . " and Genre: " . $_REQUEST['genre'] . "</em>";
-    // echo "<br><br>";
-    // echo "<em>(SQL: " . $sql . "</em>)";
-    // echo "<br><br>";
     echo "<em>Your results returned <strong>" .
         $results->num_rows .
         "</strong> results.</em>";
@@ -90,17 +113,11 @@ if($mysql->connect_errno) {
 
     while($currentrow = $results->fetch_assoc()) {
         echo "<div Class='className'><strong>" .
-            $currentrow['className'] .
-            "</strong>" .
-            " (<em>School: " . $currentrow['courseID'] . "</em>) </div>" .
+            $currentrow['className'] . "</strong>" .
+            " (<em>" . $currentrow['courseID'] . "</em>)" .
 
-            /* $currentrow["className"] . "'>" .
-            "View</a>" .
-            " | " .
-            "<a href='edit_drilldown_finished.php?recordid=" .
-            */
-
-            "<br style='clear:both;'>";
+            "<a href='funclasses_detail.php?recordid=" . $currentrow["fun_classes_id"]. "'>".
+            "[View Class Details]". "</a>"."</div>"."<br>";
 
     }
     ?>
