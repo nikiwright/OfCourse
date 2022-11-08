982feaabc57a76d1eadfe6c0d2134e646691dcef
anyac18 <94502023+anyac18@users.noreply.github.com>
2022-11-08 12:53:11 -0800

    Update funclasses_search.php

--- funclasses_search.php
+++ funclasses_search.php
@@ -18,127 +18,140 @@ if($mysql->connect_errno) {
 <htmL>
 <title>Search Page</title>
 <header>
-    <style>
-
-        body {
-            background-color: white;
-        }
-        #container{
-            width: 80%;
-            margin: auto;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-evenly;
-            flex-direction: column;
-            text-align: center;
-
-            background-color: white;
-            border: solid 1px lightgrey;
-
-        }
-
-    </style>
+    <link rel="stylesheet" href="style.css">
 </header>
 
 <body>
 
-<div id="container">
-    <h1> Search page </h1>
+<div id="header">
+    <h1 style="margin-bottom: -10px;">OF COURSE</h1><br>
 
-    <form action="funclasses_result.php">
-      Class Title:<input type="text" name="className">
-        <br style="clear:both;">
+    <p1>Fun Classes at USC</p1>
+    <br>
+    <div id="nav">
+        <a href="userprofile.php" class="navitem">
+            USER PROFILE
+        </a><br>
+        <a href="wellnessblog.php" class="navitem">
+            WELLNESS BLOG
+        </a><br>
+        <a href="sitepurpose.php" class="navitem">
+            SITE PURPOSE
+        </a><br>
+    </div>
+</div>
+<br>
 
-     CourseID:<input type="text" name="courseID">
-        <br style="clear:both;">
 
-     Department:<input type="text" name="classDepartment">
-     <br style="clear:both;">
+<div id="mainbox">
+    <div id="box1">
+        <p1>FIND YOUR NEW FAVORITE CLASS TODAY</p1>
+    </div>
+    <div id="box2">
+        <form action="funclasses_result.php">
+        <div id="textinputs">
+                Class Title: <input type="text" name="className" style="width: 100px;">
+                <br style="clear:both;">
 
-     Instructor Name:<input type="text" name="instructorName">
-     <br style="clear:both;">
+                CourseID: <input type="text" name="courseID" style="width: 100px;">
+                <br style="clear:both;">
 
-   School: <select name="school">
-        <option value="ALL">Select a school</option>
-        <?php
+                Department: <input type="text" name="classDepartment" style="width: 100px;">
+                <br style="clear:both;">
 
-        $sql = "SELECT * FROM schools
-                WHERE school != '' AND school != ' ' 
-                 ORDER BY school";
+                Instructor Name: <input type="text" name="instructorName" style="width: 100px;">
+                <br style="clear:both;">
+            </div>  <br><space></space><space></space><space></space><space></space><br>
 
-        $results = $mysql->query($sql);
+         <div id="dropdowns">
+             <select name="interest" class="searchselect">
+                 <option value="ALL">Interests</option>
+                 <?php
 
-        if(!$results) {
-            echo "SQL error: ". $mysql->error;
-            exit();
-        }
-
-        while($currentrow = $results->fetch_assoc()) {
-            echo "<option value='" . $currentrow['school_id'] . "'>" . $currentrow['school'] . "</option>";
-        }
-        ?>
-    </select>
-    <br style="clear:both;">
-
-    Area of Interest: <select name="interest">
-        <option value="ALL">Select an area of interest</option>
-        <?php
-
-        $sql = "SELECT * FROM interests
+                 $sql = "SELECT * FROM interests
                 WHERE interest != '' AND interest != ' ' 
                  ORDER BY interest";
 
-        $results = $mysql->query($sql);
+                 $results = $mysql->query($sql);
 
-        if(!$results) {
-            echo "SQL error: ". $mysql->error;
-            exit();
-        }
+                 if(!$results) {
+                     echo "SQL error: ". $mysql->error;
+                     exit();
+                 }
 
-        while($currentrow = $results->fetch_assoc()) {
-            echo "<option value='" . $currentrow['interest_id'] . "'>" . $currentrow['interest'] . "</option>";
-        }
-        ?>
-    </select>
-    <br style="clear:both;">
+                 while($currentrow = $results->fetch_assoc()) {
+                     echo "<option value='" . $currentrow['interest_id'] . "'>" . $currentrow['interest'] . "</option>";
+                 }
+                 ?>
+             </select>
 
-    Units: <select name="unit">
-        <option value="ALL">Select the number of units</option>
-        <?php
+             <select name="school" class="searchselect">
+                 <option value="ALL">School</option>
+                 <?php
 
-        $sql = "SELECT * FROM units
+                 $sql = "SELECT * FROM schools
+                WHERE school != '' AND school != ' ' 
+                 ORDER BY school";
+
+                 $results = $mysql->query($sql);
+
+                 if(!$results) {
+                     echo "SQL error: ". $mysql->error;
+                     exit();
+                 }
+
+                 while($currentrow = $results->fetch_assoc()) {
+                     echo "<option value='" . $currentrow['school_id'] . "'>" . $currentrow['school'] . "</option>";
+                 }
+                 ?>
+             </select>
+
+             <select name="unit" class="searchselect">
+                 <option value="ALL">Units</option>
+                 <?php
+
+                 $sql = "SELECT * FROM units
                 WHERE unit_num != '' AND unit_num != ' ' 
                  ORDER BY unit_num";
 
-        $results = $mysql->query($sql);
+                 $results = $mysql->query($sql);
 
-        if(!$results) {
-            echo "SQL error: ". $mysql->error;
-            exit();
-        }
+                 if(!$results) {
+                     echo "SQL error: ". $mysql->error;
+                     exit();
+                 }
 
-        while($currentrow = $results->fetch_assoc()) {
-            echo "<option value='" . $currentrow['unit_id'] . "'>" . $currentrow['unit_num'] . "</option>";
-        }
-        ?>
-    </select>
-    <br style="clear:both;">
+                 while($currentrow = $results->fetch_assoc()) {
+                     echo "<option value='" . $currentrow['unit_id'] . "'>" . $currentrow['unit_num'] . "</option>";
+                 }
+                 ?>
+             </select>
 
-        Weekdays:
-        <input type="checkbox" id="monday" name="monday" value="monday">
-        <label for="monday"> Monday</label>
-        <input type="checkbox" id="tuesday" name="tuesday" value="tuesday">
-        <label for="tuesday"> Tuesday</label>
-        <input type="checkbox" id="wednesday" name="wednesday" value="wednesday">
-        <label for="wednesday"> Wednesday</label>
-        <input type="checkbox" id="thursday" name="thursday" value="thursday">
-        <label for="thursday"> Thursday</label>
-        <input type="checkbox" id="friday" name="friday" value="friday">
-        <label for="friday"> Friday</label><br>
-        <br style="clear:both;">
+         </div>
 
-   <input type="submit" value="Search">
+            <br><space></space><space></space><space></space><space></space><br>
+
+            <div id="checkboxes">
+                <input type="checkbox" id="monday" name="monday" value="Monday">
+                <label for="monday">Monday</label><br>
+                <input type="checkbox" id="tuesday" name="tuesday" value="Tuesday">
+                <label for="tuesday">Tuesday</label><br>
+                <input type="checkbox" id="wednesday" name="wednesday" value="Wednesday">
+                <label for="wednesday">Wednesday</label><br>
+                <input type="checkbox" id="thursday" name="thursday" value="Thursday">
+                <label for="thursday">Thursday</label><br>
+                <input type="checkbox" id="friday" name="friday" value="Friday">
+                <label for="friday">Friday</label><br>
+            </div>
+            <br><space></space><space></space><space></space><space></space><br>
+
+            <input type="submit" value="Submit" id="submit" class="button">
+        </form>
+    </div>
 </div>
-</form>
+<div id="text1">
+    <h2>LOOKING TO SWITCH UP YOUR SCHEDULE?</h2><br>
+    <p2>OfCourse believes in the importance of taking advantage of the wide range of classes that USC has to offer. Taking classes outside your major/minor requirements can be a great opportunity for growth and relaxation. But we know that a hindrance for many students is not knowing which classes to take. OfCourse is a tool made by students for students to help you find courses that boost your physical and mental wellbeing.</p2>
+</div>
 </body>
 </htmL>
