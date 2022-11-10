5effc069ce69752602dc5951733af833b29bf2b5
anyac18 <94502023+anyac18@users.noreply.github.com>
2022-11-10 01:11:09 -0800

    Update funclasses_search.php

--- funclasses_search.php
+++ funclasses_search.php
@@ -22,23 +22,23 @@ if($mysql->connect_errno) {
 </header>
 
 <body>
+<div id="nav">
+    <br>&nbsp
+    <img src="Of-Course-Logo.png" id="logo">
+    <a href="userprofile.php" class="navitem">
+        USER PROFILE
+    </a><br>
+    <a href="wellnessblog.php" class="navitem">
+        WELLNESS BLOG
+    </a><br>
+    <a href="sitepurpose.php" class="navitem">
+        SITE PURPOSE
+    </a><br>
+</div>
 
 <div id="header">
-    <h1 style="margin-bottom: -10px;">OF COURSE</h1><br>
-
-    <p1>Fun Classes at USC</p1>
+    <h1 style="margin-bottom: -10px;">FUN CLASSES AT USC</h1><br>
     <br>
-    <div id="nav">
-        <a href="userprofile.php" class="navitem">
-            USER PROFILE
-        </a><br>
-        <a href="wellnessblog.php" class="navitem">
-            WELLNESS BLOG
-        </a><br>
-        <a href="sitepurpose.php" class="navitem">
-            SITE PURPOSE
-        </a><br>
-    </div>
 </div>
 <br>
 
@@ -151,7 +151,8 @@ if($mysql->connect_errno) {
 </div>
 <div id="text1">
     <h2>LOOKING TO SWITCH UP YOUR SCHEDULE?</h2><br>
-    <p2>OfCourse believes in the importance of taking advantage of the wide range of classes that USC has to offer. Taking classes outside your major/minor requirements can be a great opportunity for growth and relaxation. But we know that a hindrance for many students is not knowing which classes to take. OfCourse is a tool made by students for students to help you find courses that boost your physical and mental wellbeing.</p2>
+    <p1>OfCourse believes in the importance of taking advantage of the wide range of classes that USC has to offer. Taking classes outside your major/minor requirements can be a great opportunity for growth and relaxation. But we know that a hindrance for many students is not knowing which classes to take. OfCourse is a tool made by students for students to help you find courses that boost your physical and mental wellbeing.</p1>
+<br><br><br><br>
 </div>
 </body>
 </htmL>
