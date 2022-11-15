b69df944955c78efd26ecd54f69f752d90f1bbfc
anyac18 <94502023+anyac18@users.noreply.github.com>
2022-11-15 02:31:18 -0800

    Update funclasses_detail.php

--- funclasses_detail.php
+++ funclasses_detail.php
@@ -20,47 +20,53 @@ if($mysql->connect_errno) {
 <html>
 <title> Details Page </title>
 <header>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="style.css">
 </header>
-<body id="body2">
-<div id="resultbox">
-<?php
+<body id="body3">
+<div id="container">
+<img src="Of-Course-Logo.png" id="logo">
 
-if (empty($_REQUEST["recordid"])){
-    echo "Error! please go through the search page";
-    exit();
-}
+    <?php
 
-$sql = "SELECT * from generalView
+    if (empty($_REQUEST["recordid"])){
+        echo "Error! please go through the search page";
+        exit();
+    }
+
+    $sql = "SELECT * from generalView
          WHERE fun_classes_id =" .
-    $_REQUEST['recordid'];
+        $_REQUEST['recordid'];
 
-//echo "SQL: ". $sql. "<br>"."<br>";
+    //echo "SQL: ". $sql. "<br>"."<br>";
 
-$results = $mysql -> query($sql);
+    $results = $mysql -> query($sql);
 
-if(!$results){
-    echo "ERROR: " . $mysql -> error;
-}
+    if(!$results){
+        echo "ERROR: " . $mysql -> error;
+    }
 
-while ($currentrow = $results -> fetch_assoc()){
-    echo "<h1>". $currentrow["className"]. " Details" ."</h1>";
-    echo "<strong>"."Course Title: " ."</strong>". $currentrow["className"]. "<br>";
-    echo "<strong>"."CourseID: " ."</strong>". $currentrow["courseID"]. "<br>";
-    echo "<strong>"."About: " ."</strong>". $currentrow["classBio"]. "<br>";
-    echo "<strong>"."Days offered: " ."</strong>". $currentrow["weekday"]. "<br>";
-    echo "<strong>"."Units: " ."</strong>". $currentrow["unit_num"]. "<br>"."<br>";
+    while ($currentrow = $results -> fetch_assoc()){
+        echo "<h1 id='resultheader'>". $currentrow["className"]. " Details" ."</h1> <br>";
+        echo "<div id='transparentbox2'>
+<div id='resultbox'>";
+        echo "<strong>"."Course Title: " ."</strong>". $currentrow["className"]. "<br>";
+        echo "<strong>"."CourseID: " ."</strong>". $currentrow["courseID"]. "<br>";
+        echo "<strong>"."About: " ."</strong>". $currentrow["classBio"]. "<br>";
+        echo "<strong>"."Days offered: " ."</strong>". $currentrow["weekday"]. "<br>";
+        echo "<strong>"."Units: " ."</strong>". $currentrow["unit_num"]. "<br>"."<br>";
 
-    echo "<strong>"."Class Department: " ."</strong>". $currentrow["classDepartment"]. "<br>";
-    echo "<strong>"."School: " ."</strong>". $currentrow["school"]. "<br>"."<br>";
+        echo "<strong>"."Class Department: " ."</strong>". $currentrow["classDepartment"]. "<br>";
+        echo "<strong>"."School: " ."</strong>". $currentrow["school"]. "<br>"."<br>";
 
-    echo "<strong>"."Instructor Name: " ."</strong>". $currentrow["instructorName"]. "<br>";
-    echo "<strong>"."Instructor Rating: " ."</strong>". $currentrow["instructorRating"]. "<br>"."<br>";
+        echo "<strong>"."Instructor Name: " ."</strong>". $currentrow["instructorName"]. "<br>";
+        echo "<strong>"."Instructor Rating: " ."</strong>". $currentrow["instructorRating"]. "<br>"."<br>";
 
-    echo "<strong>"."Course Reviews: " ."</strong>";
-}
+        echo "<strong>"."Course Reviews: " ."</strong>";
+    }
 
-?>
+    ?>
+</div>
+</div>
 </div>
 </body>
 </html>
