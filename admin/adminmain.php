6082761d332672f480984d834dc94da62f9d8d7e
sssarma <sssarma@usc.edu>
2022-11-21 23:43:11 -0800

    main admin

--- admin/adminmain.php
+++ admin/adminmain.php
@@ -25,41 +25,17 @@ if($mysql->connect_errno) {
 <div id="nav">
     <br>&nbsp
     <img src="./css/Of-Course-Logo.png" id="logo">
-    <a href="userprofile_add.php" class="navitem">
-        ADD USER PROFILE
+    <a href="" class="navitem">
+        USER PROFILES
     </a><br>
-    <a href="userprofile_edit.php" class="navitem">
-        EDIT USER PROFILE
+    <a href="" class="navitem">
+        WELLNESS BLOG
     </a><br>
-    <a href="userprofile_delete.php" class="navitem">
-        DELETE USER PROFILE
+    <a href="" class="navitem">
+        CLASSES
     </a><br>
-    <a href="wellnessblog_add.php" class="navitem">
-        ADD WELLNESS BLOG
-    </a><br>
-    <a href="wellnessblog_edit.php" class="navitem">
-        EDIT WELLNESS BLOG
-    </a><br>
-    <a href="wellnessblog_delete.php" class="navitem">
-        DELETE WELLNESS BLOG
-    </a><br>
-    <a href="class_add.php" class="navitem">
-        ADD CLASS
-    </a><br>
-    <a href="class_edit.php" class="navitem">
-        EDIT CLASS
-    </a><br>
-    <a href="class_delete.php" class="navitem">
-        DELETE CLASS
-    </a><br>
-    <a href="review_add.php" class="navitem">
-        ADD REVIEW
-    </a><br>
-    <a href="review_edit.php" class="navitem">
-        EDIT REVIEW
-    </a><br>
-    <a href="review_delete.php" class="navitem">
-        DELETE REVIEW
+    <a href="" class="navitem">
+        REVIEWS
     </a><br>
 </div>
 
