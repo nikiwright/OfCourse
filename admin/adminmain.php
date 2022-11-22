e864f8e8ef7cb4fda01af5f2ba46a0756d733483
sssarma <sssarma@usc.edu>
2022-11-21 23:54:24 -0800

    main admin

--- admin/adminmain.php
+++ admin/adminmain.php
@@ -26,13 +26,37 @@ if($mysql->connect_errno) {
     <br>&nbsp
     <img src="../css/Of-Course-Logo.png" id="logo">
     <div class="dropdown">
-        <button class="dropbtn">Dropdown
+        <button class="dropbtn">USER PROFILES
             <i class="fa fa-caret-down"></i>
         </button>
         <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+            <a href="userprofile_add.php">ADD</a>
+            <a href="userprofile_edit.php">EDIT</a>
+            <a href="userprofile_delete.php">DELETE</a>
+        </div>
+        <button class="dropbtn">WELLNESS BLOG
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="wellnessblog_add.php">ADD</a>
+            <a href="wellnessblog_edit.php">EDIT</a>
+            <a href="wellnessblog_delete.php">DELETE</a>
+        </div>
+        <button class="dropbtn">CLASSES
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="class_add.php">ADD</a>
+            <a href="class_edit.php">EDIT</a>
+            <a href="class_delete.php">DELETE</a>
+        </div>
+        <button class="dropbtn">REVIEWS
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="review_add.php">ADD</a>
+            <a href="review_edit.php">EDIT</a>
+            <a href="review_delete.php">DELETE</a>
         </div>
     </div>
     <a href="" class="navitem">
