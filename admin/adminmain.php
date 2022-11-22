1407686a898f1b1ad429269f89447316874b0eb4
sssarma <sssarma@usc.edu>
2022-11-21 23:10:12 -0800

    main admin

--- admin/adminmain.php
+++ admin/adminmain.php
@@ -31,9 +31,6 @@ if($mysql->connect_errno) {
     <a href="wellnessblog.php" class="navitem">
         WELLNESS BLOG
     </a><br>
-    <a href="sitepurpose.php" class="navitem">
-        SITE PURPOSE
-    </a><br>
     <a href="class_edit.php.php" class="navitem">
         CLASSES
     </a><br>
