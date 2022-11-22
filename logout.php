006a3339ff001137beaaf6249d4c1a9b8da079aa
nikiwright <ddwright@usc.edu>
2022-11-22 00:01:18 -0800

    login page, logout page, site navigation update, user profile page

--- logout.php
+++ logout.php
@@ -13,26 +13,51 @@ if($mysql->connect_errno) {
     echo "db connection error : " . $mysql->connect_error;
     exit();
 }
+
+include 'sitenav.php';
+
 ?>
 
 <html>
 <header>
     <title>Log Out Page</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <style>
+        body {
+            background-color:#9BA2FF;
+        }
+
+        #transparentbox {
+            text-align: center;
+            width: 30%;
+            font-size: 16pt;
+        }
+        #a {
+            color: black;
+         font-weight: bold;
+        }
+
+    </style>
 </header>
 <body>
-<?php
-// logout routine
-session_start();
-//unset($_SESSION["loggedin"]);
+<div id="transparentbox">
+    <?php
+    // logout routine
+    session_start();
+    //unset($_SESSION["loggedin"]);
 
-session_unset();
+    session_unset();
 
-session_destroy();
-echo "You have successfully LOGGED OUT". "<br>";
-echo "Log back in? Click here: ". "<a href='login.php'>". "LOG IN". "</a>"."<br>";
+    session_destroy();
+    echo "You have successfully LOGGED OUT". "<br>"."<br>";
+    echo "We are sad to see you go. Want to keep finding fun classes?"."<br>"."<br>".
+        " Click ". "<a id='a' href='login.php'>". "here". "</a>". " to log back in."."<br>";
 
-echo print_r($_SESSION);
-?>
+//    echo print_r($_SESSION);
+    ?>
+
+</div>
+
 
 </body>
 </html>
---
+++ user_profile.php
@@ -0,0 +1,83 @@
+<?php
+
+include 'nwloginvariables.php';
+
+$mysql = new mysqli(
+    $host,
+    $userid,
+    $userpw,
+    $db
+);
+
+if($mysql->connect_errno) {
+    echo "db connection error : " . $mysql->connect_error;
+    exit();
+}
+
+session_start();   // session starts
+?>
+
+
+<html>
+<head>
+    <title> User Profile </title>
+    <link rel="stylesheet" href="./css/style.css">
+    <style>
+        body {
+            background-color:#9BA2FF;
+        }
+
+        #box1 {
+            background-color:#FFC72C;
+            color: black;
+        }
+
+        #a {
+            color: black;
+            font-weight: bold;
+        }
+
+        #submit {
+            background-color: #FF5E5B;
+        }
+
+    </style>
+</head>
+<body>
+<?php
+include 'sitenav.php';
+?>
+
+<!--Session Variables: <em>--><?//= print_r($_SESSION) ?><!--</em>-->
+
+<h1 id="resultheader">USER PROFILE</h1><br>
+<div id="mainbox">
+    <div id="box1">
+        <?php
+        if ($_SESSION['loggedin'] == "yes") {
+       echo "Hello ". $_SESSION['first'] . "!". "<br>". "You are logged in.";
+        } else {
+           echo "You are not logged in. Click" ."<a id='a' href='login.php'> here </a>" . " to log in/signup!";
+        }
+        ?>
+    </div>
+    <div id="box2">
+        <?php
+        if ($_SESSION['loggedin'] == "yes") {
+            echo "First Name: " . $_SESSION['first'] . "<br>" .
+                "Last Name: " . $_SESSION['last'] . "<br>" .
+                "Username: " . $_SESSION['username'] . "<br>";
+
+            echo "<a href='logout.php'>"."<br>".
+                "<input type='submit' name='logout' value='LOGOUT' id='submit' class='button'>".
+                "</a>";
+
+        } else {
+         echo "Log in/ sign up to view your profile!";
+        }
+        ?>
+
+    </div>
+</div>
+</body>
+</html>
