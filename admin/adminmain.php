e3bc43989504b7bc0e4c6b36be786334e55eeff3
sssarma <sssarma@usc.edu>
2022-11-21 22:23:17 -0800

    main admin

--- admin/adminmain.php
+++ admin/adminmain.php
@@ -52,6 +52,8 @@ if($mysql->connect_errno) {
 <div id="mainbox">
     <div id="box1">
         <p1>EDITING OPTIONS</p1>
+        <p1><strong>Classes</strong></p1><br>
+        <a href="class_edit.php"></a>
     </div>
     <div id="box2">
     <p1><strong>Classes</strong></p1><br>
