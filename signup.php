220f21b33f080e930a2a7670114a06057074f0d3
nikiwright <ddwright@usc.edu>
2022-11-22 15:43:59 -0800

    signup page

--- signup.php
+++ signup.php
@@ -54,8 +54,6 @@ include 'sitenav.php';
     <div id="box2">
 
         <form action ="" method="post">
-
-
             <table width="250" border="0">
                 <tr>
                     <td> First Name: </td>
@@ -84,28 +82,92 @@ include 'sitenav.php';
         </form>
 
     <?php
-    $sql = "INSERT INTO users 
-    (user_firstName, user_lastName, username, password) 
-    
-    VALUES ('" . $_POST['firstName'] . "',
-    '" . $_POST['lastName'] . "',
-    '" . $_POST['username'] . "',
-    '" . $_POST['password'] . "' )";
 
-    $results = $mysql->query($sql);
 
-    if (!$results) {
-        echo "<hr>Your SQL:<br> " . $sql . "<br><br>";
-        echo "SQL Error: " . $mysql->error . "<hr>";
-        exit();
-    } else {
-        echo $results;
+    if ($_SESSION['loggedin'] == "yes")   // Checking whether the session is already there or not
+    {
+        // all good
+//            echo "Logged in!";
+//            print_r($_SESSION);
+        header('Location:user_profile.php');
 
     }
 
+      if ((empty($_POST['firstName']))
+        &(empty($_POST['lastName']))
+        &(empty($_POST['username']))
+        &(empty($_POST['password']))) {
+        echo "";
+        exit();
+    } else {
 
+          $sql2 = "SELECT * FROM users WHERE 
+                        username='" . $_POST['username'] . "'";
+
+          $results2 = $mysql->query($sql2);
+
+          if (!$results2) {
+              echo "<hr>Your SQL:<br> " . $sql2 . "<br><br>";
+              echo "SQL Error: " . $mysql->error . "<hr>";
+              exit();
+          }
+
+          if ($results2->num_rows === 1) {
+
+              $row2 = mysqli_fetch_array($results2);
+
+              if ($row2['username'] ===  $_POST['username']) {
+                  echo "Sorry! This username is already taken, try another one. If this is your account, " .
+                      "click" . "<a href='login.php'> here </a>" . " to log in!";
+                  exit();
+
+              } else {
+                  $sql = "INSERT INTO users 
+                 (user_firstName, user_lastName, username, password) 
+    
+                 VALUES ('" . $_POST['firstName'] . "',
+                 '" . $_POST['lastName'] . "',
+                 '" . $_POST['username'] . "',
+                 '" . $_POST['password'] . "' )";
+
+                  $results = $mysql->query($sql);
+
+                  if (!$results) {
+                      echo "<hr>Your SQL:<br> " . $sql . "<br><br>";
+                      echo "SQL Error: " . $mysql->error . "<hr>";
+                      exit();
+                  } else {
+//                      echo $results;
+                      echo "You have successfully created an account! ";
+                      echo "Click" . "<a href='login.php'> here </a>" . " to log in!";
+                  }
+
+              }
+
+          } else {
+              $sql = "INSERT INTO users 
+                 (user_firstName, user_lastName, username, password) 
+    
+                 VALUES ('" . $_POST['firstName'] . "',
+                 '" . $_POST['lastName'] . "',
+                 '" . $_POST['username'] . "',
+                 '" . $_POST['password'] . "' )";
+
+              $results = $mysql->query($sql);
+
+              if (!$results) {
+                  echo "<hr>Your SQL:<br> " . $sql . "<br><br>";
+                  echo "SQL Error: " . $mysql->error . "<hr>";
+                  exit();
+              } else {
+//                  echo $results;
+                  echo "You have successfully created an account! ";
+                  echo "Click" . "<a href='login.php'> here </a>" . " to log in!";
+              }
+
+          }
+      }
     ?>
-
 
     </div>
 </div>
