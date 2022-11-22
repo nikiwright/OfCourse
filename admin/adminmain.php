5df4991c8fb78fa733699c41c1a6952b6999acc1
sssarma <sssarma@usc.edu>
2022-11-21 23:47:44 -0800

    main admin

--- admin/adminmain.php
+++ admin/adminmain.php
@@ -25,6 +25,16 @@ if($mysql->connect_errno) {
 <div id="nav">
     <br>&nbsp
     <img src="./css/Of-Course-Logo.png" id="logo">
+    <div class="dropdown">
+        <button class="dropbtn">Dropdown
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+    </div>
     <a href="" class="navitem">
         USER PROFILES
     </a><br>
