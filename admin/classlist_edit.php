d6d973125059a83061535e7ff981f0393642553d
sssarma <sssarma@usc.edu>
2022-12-02 01:55:08 -0800

    class edit css

--- admin/classlist_edit.php
+++ admin/classlist_edit.php
@@ -29,6 +29,9 @@ if($mysql->connect_errno) {
             background-color:#FFC72C;
             color: black;
         }
+        #box2 {
+            width: auto;
+        }
 
         #a {
             color: black;
