789cd41bc836e8dd4af9ba7c0f3668cba4b93140
anyac18 <aschung@usc.edu>
2022-12-02 18:33:23 -0800

    Wellness blog and site purpose updates

--- sitepurpose.php
+++ sitepurpose.php
@@ -32,13 +32,21 @@ if($mysql->connect_errno) {
 include 'sitenav.php';
 ?>
 
-WHAT IS OFCOURSE? <br><br>
+<div id="detailbox" style="width: 60%; margin:auto;">
+    <div id="box1" style="background-color: white">
+        <h1 id="resultheader" style="color: black; margin-top: 10px">
+            WHAT IS OF COURSE?
+        </h1>
+    </div>
+    <div id="details"><br>
+        OfCourse is a tool made by students for students to help you find courses that boost your physical and mental wellbeing. <br><br>
 
-OfCourse is a tool made by students for students to help you find courses that boost your physical and mental wellbeing. <br><br>
+        OfCourse believes in the importance of taking advantage of the wide range of classes that USC has to offer.
+        Taking classes outside your major/minor requirements can be a great opportunity for growth and relaxation.
+        But we know that a hindrance for many students is not knowing which classes to take.
+    </div>
+</div>
 
-OfCourse believes in the importance of taking advantage of the wide range of classes that USC has to offer.
-Taking classes outside your major/minor requirements can be a great opportunity for growth and relaxation.
-But we know that a hindrance for many students is not knowing which classes to take.
 
 
 </body>
--- wellnessblog.php
+++ wellnessblog.php
@@ -23,16 +23,16 @@ include 'sitenav.php';
 </div>
 <div id="resultbox" style="padding: 0px !important; text-align: center">
     <div id="box1" style="background-color: #FFC72C !important;">
-        <h2 style="line-height: 0px; margin-top: 10px;">Date:</h2>
+        <h2 style="line-height: 0px; margin-top: 10px;">Date: <?php echo date("l F jS, Y"); ?> </h2>
     </div>
     <div class="wellnesstext">"Life begins at the end of your comfort zone." - Neale Donald Walsch</div>
     <div id="box1" style="background-color: #FF5E5B !important; border-radius: 0px !important; border-top: 3px black solid">
-        <h2 style="line-height: 0px; margin-top: 10px;">Date:</h2>
+        <h2 style="line-height: 0px; margin-top: 10px;">Date: <?php echo date("l F jS, Y"); ?> </h2>
     </div>
     <img src="./css/funclassimage1.png" class="wellnessimage" ><br>
     <div class="wellnesstext" style="margin-top: 20px !important; margin-left: 60px; margin-right: 60px;">Intro to Standup Comedy allows you to participate in all sorts of acting exercises that push you outside of your comfort zone. Students work towards an end-of-semester performance in front of friends and family. This semester’s THTR 474 classes will be showcasing their newfound talents on December 2nd. Come out for a night of fun and laughter!</div>
     <div id="box1" style="background-color: #7BC950 !important; border-radius: 0px !important; border-top: 3px black solid">
-        <h2 style="line-height: 0px; margin-top: 10px;">Date:</h2>
+        <h2 style="line-height: 0px; margin-top: 10px;">Date: <?php echo date("l F jS, Y"); ?> </h2>
     </div>
     <div class="wellnesstext" style="margin-top: 20px !important;"><a href="https://www.huffpost.com/entry/happy-in-college_n_5525905">
             Take classes that reflect and further your interests
